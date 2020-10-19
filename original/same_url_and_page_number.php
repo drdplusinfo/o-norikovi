62b7a77b50a1e0617586063292ffe8c35b581c49
Jaroslav Týc <mail@jaroslavtyc.com>
2020-10-19 10:54:42 +0200

    Fixed some invalid links to next section

--- original/same_url_and_page_number.php
+++ original/same_url_and_page_number.php
@@ -2,12 +2,13 @@
 \error_reporting(-1);
 if ((!empty($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] === '127.0.0.1') || PHP_SAPI === 'cli') {
     \ini_set('display_errors', '1');
+    error_reporting(-1);
 } else {
     \ini_set('display_errors', '0');
 }
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$filesToPageNumbers = [];
+$filesToSectionNumbers = [];
 $expectedNonDigitSectionHrefs = ['../konec'];
 $expectedNonDigitSectionNames = ['KONEC'];
 $autoriBaseName = basename(__DIR__ . '/../web/autori/autoři.html');
@@ -36,14 +37,17 @@ HTML
     foreach ($anchors as $anchor) {
         $href = (string)$anchor->getAttribute('href');
         if (!preg_match('~^[.]{2}/\d+$~', $href) && !in_array($href, $expectedNonDigitSectionHrefs, true)) {
-            trigger_error(sprintf("Strange href, expected something like '../123', got '$href'"), E_USER_ERROR);
-            continue;
+            trigger_error(sprintf("Strange href, expected something like '../123', got '%s'", $href), E_USER_ERROR);
         }
         $sectionNumber = trim((string)$anchor->prop_get_innerText());
         if (!preg_match('~^\d+$~', $sectionNumber) && !in_array($sectionNumber, $expectedNonDigitSectionNames)) {
-            trigger_error(sprintf("Strange section number in a link text, expected something like '123', got '$sectionNumber'"), E_USER_ERROR);
-            continue;
+            trigger_error(sprintf("Strange section number in a link text, expected something like '123', got '%s'", $sectionNumber), E_USER_ERROR);
         }
-        $filesToPageNumbers[$href] = $sectionNumber;
+        if (!array_key_exists($href, $filesToSectionNumbers)) {
+            $filesToSectionNumbers[$href] = $sectionNumber;
+        } elseif ($filesToSectionNumbers[$href] !== $sectionNumber) {
+            trigger_error(sprintf("Href '%s' is already pointing to '%s' and now is pointing to '%s'?", $href, $filesToSectionNumbers[$href], $sectionNumber), E_USER_ERROR);
+        }
     }
+//    print_r($filesToSectionNumbers);
 }
