61748a6a372baf3ea587f2548bed5ad964daa69d
Jaroslav Týc <mail@jaroslavtyc.com>
2020-10-20 13:48:49 +0200

    Cover is stretched as mutch as possible

--- js/show-previous-visit-page.js
+++ js/show-previous-visit-page.js
@@ -7,24 +7,29 @@ showPreviousVisitPage = () => {
             previouslyVisitedPage = Number.parseInt(previouslyVisitedPageCookie.split('=')[1])
         }
     }
-    if (previouslyVisitedPage) {
-        const firstPage = Number.parseInt(document.getElementById('new_adventure').href.replace(/^[^0-9]+/, ''))
-        if (previouslyVisitedPage === firstPage) {
-            return
-        }
-        const continueContainer = document.getElementById('continue_from_previous_visit_page')
-        if (!continueContainer) {
-            console.warn("Can not found element with ID 'continue_from_previous_visit_page_element'")
-            return
-        }
-        const continueAnchor = continueContainer.getElementsByTagName('a').item(0)
-        if (!continueAnchor) {
-            console.warn("Can not found anchor in element with ID 'continue_from_previous_visit_page_element'")
-            return
-        }
-        continueAnchor.href = `/${previouslyVisitedPage}`
-        continueContainer.classList.remove('hidden')
+    if (!previouslyVisitedPage) {
+        return
     }
+    const newAdventureElement = document.getElementById('new_adventure')
+    if (!newAdventureElement) {
+        return
+    }
+    const firstPage = Number.parseInt(newAdventureElement.href.replace(/^[^0-9]+/, ''))
+    if (previouslyVisitedPage === firstPage) {
+        return
+    }
+    const continueContainer = document.getElementById('continue_from_previous_visit_page')
+    if (!continueContainer) {
+        console.warn("Can not found element with ID 'continue_from_previous_visit_page_element'")
+        return
+    }
+    const continueAnchor = continueContainer.getElementsByTagName('a').item(0)
+    if (!continueAnchor) {
+        console.warn("Can not found anchor in element with ID 'continue_from_previous_visit_page_element'")
+        return
+    }
+    continueAnchor.href = `/${previouslyVisitedPage}`
+    continueContainer.classList.remove('hidden')
 }
 
 showPreviousVisitPage()
