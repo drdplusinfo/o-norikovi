8ef788f1b7b08659387f9048ece893771cbb6c1d
Jaroslav Týc <mail@jaroslavtyc.com>
2020-11-08 08:23:37 +0100

    Local links are prefetched and cached in browser

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php
+header('Cache-Control: public');
+header('Cache-Control: max-age=60');
 require __DIR__ . '/vendor/drdplus/rules-skeleton/index.php';
---
+++ js/quicklink.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // https://github.com/GoogleChromeLabs/quicklink
+    quicklink.listen({
+        priority: false,
+        timeout: 3000
+    })
+})
