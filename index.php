c6f0a4f800ab2935e99911dc72f8f2fd4753d17c
Jaroslav Týc <mail@jaroslavtyc.com>
2020-11-09 09:31:40 +0100

    Cache control headers are sent only if content seems OK

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-header('Cache-Control: public');
-header('Cache-Control: max-age=60');
+ob_start();
 require __DIR__ . '/vendor/drdplus/rules-skeleton/index.php';
+$output = ob_get_clean();
+if ($output && strpos($output, '<div class="o-norikovi">') !== false) {
+    header('Cache-Control: public');
+    header('Cache-Control: max-age=600');
+}
+echo $output;
